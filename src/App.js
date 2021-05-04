bfe64bf61eed451fce18c5277d894f394b9c5694
HALEY_BUBLEY <HaleyBubley@gmail.com>
2021-05-04 10:25:24 -0500

    Beginning clean ups and function testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [alert, setAlert] = useState(null);
+
+    const randomNumberGenerator = (range) => {
+        return Math.floor(Math.random() * (1 + range));
+    };
+
+    const fillArray = (length, range) => {
+        const randomNumberArray = [];
+        while (randomNumberArray.length < length) {
+            let randomNumber = randomNumberGenerator(range);
+            if (
+                !randomNumberArray.filter((rNum) => rNum === randomNumber)
+                    .length
+            ) {
+                randomNumberArray.push(randomNumber);
+            }
+        }
+        return randomNumberArray;
+    };
+    
+    const solutionIndices = fillArray(3, 5);
+
+    const randomNumberArray = fillArray(6, 10);
+
+    const displayNumber = () => {
+        const reducer = (accumulator, currentValue) =>
+            accumulator + randomNumberArray[currentValue];
+
+        return solutionIndices.reduce(reducer);
+    };
+    const checkInput = (userInput) => {
+        if (!userInput.length) {
+            setAlert("That's not enough for me to work with!");
+        } else {
+            userInput.map(
+                (input) =>
+                    solutionIndices.find(
+                        (sNum) => randomNumberArray[sNum] === input
+                    ) && console.log("Got one!", input)
+            );
+        }
+    };
+    checkInput([0, 2, 4]);
+    return (
+        <div className="App">
+            <span>{displayNumber()}</span>
+        </div>
+    );
 }
 
 export default App;
